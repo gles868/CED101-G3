15b69acffbd463b47a534ab86030ce9241d973e4
koalashan <cherry85105@gmail.com>
2020-12-25 17:55:05 +0800

    更新首頁 教師

--- js/index.js
+++ js/index.js
@@ -1,40 +1,59 @@
-tl1 = new TimelineMax({ease: Power2.easeOut});
-tl2 = new TimelineMax({paused:true});
+// $(function () { // wait for document ready
+    // 創建一個scrollMagic的控制器
+    var controller = new ScrollMagic.Controller();
 
-tl1.from( '.lay01', 1 , { scale: 2, y: 100 } )
-.from( '.lay02', 1 , { scale: 1.5, y: 80 }, 0 )
-.from( '.lay03', 1 , { scale: 1.3, y: 60 }, 0 )
-.from( '.lay04', 1 , { scale: 1.1, y: 0 }, 0 )
-.from( '.lay05', 1 , { scale: 1.05 }, 0 )
-.from( '.lay06', 1 , { y: 100, autoAlpha: 0 }, 0 );
+    // define movement of panels
+    var tl1 = new TimelineMax({ease: Power2.easeOut});  // 過渡效果的速度曲線（緩動效果）
+    var tl2 = new TimelineMax({paused: true});  // true:停止動畫時間軸，以滾動取代動畫的執行  // 如果設置為true，時間軸將在創建時立即暫停
 
-tl2.to( '.lay01', 1 , { scale: 1.2, y: 75, autoAlpha: 1 } )
-.to( '.lay02', 1 , { scale: 1.05, y: 50, autoAlpha: 1 }, 0 )
-.to( '.lay03', 1 , { scale: 1.025, y: 5, autoAlpha: 1 }, 0 )
-.to( '.lay04', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
-.to( '.lay05', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
-.to( '.lay06', 1 , { y: 50, autoAlpha: 0.5 }, 0 )
-.add('startFade')
-.to( '.stage', 1 , { autoAlpha: 0 }, 'startFade' )
-.to( '.lay01', 1 , { scale: 1.4, y: 150, autoAlpha: 0 }, 'startFade' )
-.to( '.lay02', 1 , { scale: 1, y: 270, autoAlpha: 0 }, 'startFade' )
-.to( '.lay03', 1 , { scale: 1.1, y: 10, autoAlpha: 0 }, 'startFade' )
-.to( '.lay04', 1 , { scale: 1.01, y: 20, autoAlpha: 0 }, 'startFade' )
-.to( '.lay05', 1 , { scale: 1.01, autoAlpha: 0 }, 'startFade' )
-.to( '.lay06', 1 , { y: 0, autoAlpha: 1 },  'startFade' );
+    // 載入頁面時的動畫 -> 倒退動作
+    tl1.from( '.lay01', 1 , { scale: 6, y: 100 } )
+    .from( '.lay02', 1 , { scale: 3, y: 80 }, 0 )
+    .from( '.lay03', 2 , { y: 100, autoAlpha: 0 }, 0 )
+    .from( '.lay04', 1 , { scale: 1.3, y: 60 }, 0 )
+    .from( '.lay05', 1 , { scale: 1.1, y: 0 }, 0 )
+    .from( '.lay06', 4 , { scale: 1.05 }, 0 );
 
-$(window).scroll( function(){
-    var st = $(this).scrollTop();  // 返回滾動條的垂直位置
-    var sh = $('.two').outerHeight();  // 容器的高(含margin)
-    console.log(st);
-    console.log(sh);
-    if( st < sh ){
-        windowScroll = st/sh;
-        if( windowScroll > 0 ){
-            tl2.progress( windowScroll );  // progress(value) -> value: 0-1; 1 means complete 
-        }				
-    }
-});
+    // 滾動時進行的動畫
+    tl2.to( '.lay01', 1 , { scale: 1.2, y: 75, autoAlpha: 1 } )
+    .to( '.lay02', 1 , { scale: 1.05, y: 50, autoAlpha: 1 }, 0 )
+    .to( '.lay03', 1 , { y: 50, autoAlpha: 0.5 }, 0 )
+    .to( '.lay04', 1 , { scale: 1.025, y: 5, autoAlpha: 1 }, 0 )
+    .to( '.lay05', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
+    .to( '.lay06', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
+    .add('startFade')
+    .to( '#pinContainer', 1 , { autoAlpha: 0 }, 'startFade' )
+    .to( '.lay01', 1 , { scale: 1.4, y: 150, autoAlpha: 0 }, 'startFade' )
+    .to( '.lay02', 1 , { scale: 1, y: 270, autoAlpha: 0 }, 'startFade' )
+    .to( '.lay03', 1 , { y: 0, autoAlpha: 1 },  'startFade' )
+    .to( '.lay04', 1 , { scale: 1.1, y: 10, autoAlpha: 0 }, 'startFade' )
+    .to( '.lay05', 1 , { scale: 1.01, y: 20, autoAlpha: 0 }, 'startFade' )
+    .to( '.lay06', 1 , { scale: 1.01, autoAlpha: 0 }, 'startFade' );
+
+    // create scene to pin and link animation
+    // new ScrollMagic.Scene({
+    //     triggerElement: "#aa",
+    //     triggerHook: 0.5,
+    //     duration: "500%"
+    // })
+    // .setPin("#pinContainer")
+    // .setTween(tl2)
+    // .addIndicators() // add indicators (requires plugin)
+    // .addTo(controller);
+
+    $(window).scroll( function(){
+        var st = $(this).scrollTop();
+        var sh = $('#pinContainer').outerHeight();
+        if( st < sh ){
+          windowScroll = st/sh;
+          if( windowScroll > 0 ){
+            tl2.progress( windowScroll );
+          }				
+        }
+    });
+
+// });
+
 
 // $(function () { // wait for document ready
 //     // 創建一個scrollMagic的控制器
@@ -60,3 +79,20 @@ $(window).scroll( function(){
 //         .addIndicators() 
 //         .addTo(controller);  // assign the scene to the controller
 // });
+
+// jQuery(function() { 
+//     var controller = new ScrollMagic.Controller(); 
+//     var tween = TweenMax.to('#boxAnim', 1, {
+//         className: '+=animate'
+//     }); 
+//     var scene = new ScrollMagic.Scene({
+//         triggerElement: '#trigger', 
+//         duration: 300, 
+//         triggerHook: 0
+//     }) 
+//     .setTween(tween) 
+//     .addTo(controller); 
+    
+//     var height = $(window).outerHeight(); 
+//     $('.height').append(height); 
+// }); 
