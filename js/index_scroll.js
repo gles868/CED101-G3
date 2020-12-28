cd6065fe4c5ec3869302604b83144101c078f0ce
koalashan <cherry85105@gmail.com>
2020-12-28 19:37:50 +0800

    header hamburger

--- js/index_scroll.js
+++ js/index_scroll.js
@@ -2,41 +2,60 @@
 let controller = new ScrollMagic.Controller();
 
 // 定義動畫
-let animate = new TimelineMax({ease: Power2.easeOut});
-let scroll = new TimelineMax(); 
+// let tl = gsap.timeline({ease: Power2.easeOut});
+let tl1= gsap.timeline({ease: Power2.easeInOut});
+let tl2= gsap.timeline({ease: Power2.easeInOut});
+let scroll = gsap.timeline(); 
 
-// 載入頁面時的動畫 -> 倒退動作
+// 載入頁面時的動畫
 // TweenMax.to( 選擇器, 時間(秒), {屬性}, 控制動畫插入點 )  
 // *控制動畫插入點 -> 無設置：時間軸末尾 | 1：絕對時間，指定時間處 ...
-animate.from('.lay01', 1.2, {scale: 4, y: 50})
-.from('.lay02', 1, {scale: 3, y: 80}, 0)
-.from('.lay03', 1, {y: 200, autoAlpha: 0}, 0)
-.from('.lay04', 1, {scale: 1.3, y: 60}, 0)
-.from('.lay05', 1, {scale: 1.1, y: 0}, 0)
-.from('.lay06', 4, {scale: 1.05}, 0);
+tl1.from( '.lay01', { delay: 0.6, scale: 3, y: 50, duration: 1.2, autoAlpha: 0 }, 0.2)
+.from( '.lay02', { delay: 0.6, scale: 3, y: 80, duration: 1.3, autoAlpha: 0 }, 0)
+.from( '.lay03', { y: 200, autoAlpha: 0, duration: 1 }, 0)
+.from( '.lay03-2', { y: 200, autoAlpha: 0, duration: 1 }, 0)
+.from( '.lay04', { delay: 0, scale: 1.3, y: 60, duration: 2, autoAlpha: 1 }, 0)
+.from( '.lay05', { delay: 0, scale: 1.3, y: 0, duration: 1.2, autoAlpha: 0.8 }, 0)
+.from( '.lay06', { delay: 0, scale: 1.05, duration: 4 }, 0);
 
 // 滾動時進行的動畫
-scroll.to( '.lay01', 3 , { scale: 1.2, y: 75, autoAlpha: 1 } )
-.to( '.lay02', 3 , { scale: 1.05, y: 50, autoAlpha: 1 }, 0 )
-.to( '.lay03', 2 , { y: -80, autoAlpha: 1 }, 3 )
-.to( '.lay04', 1 , { scale: 1.025, y: 5, autoAlpha: 1 }, 0 )
-.to( '.lay05', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
-.to( '.lay06', 1 , { scale: 1.02, autoAlpha: 1 }, 0 )
-.add('startFade')
-.to( '#pinSection', 1 , { autoAlpha: 0 }, 'startFade' )
-.to( '.lay01', 1 , { scale: 1.4, y: 150, autoAlpha: 0 }, 'startFade' )
-.to( '.lay02', 1 , { scale: 1, y: 270, autoAlpha: 0 }, 'startFade' )
-.to( '.lay03', 2 , { y: 300, autoAlpha: 1 },  'startFade' )
-.to( '.lay04', 1 , { scale: 1.1, y: 10, autoAlpha: 0 }, 'startFade' )
-.to( '.lay05', 1 , { scale: 1.01, y: 20, autoAlpha: 0 }, 'startFade' )
-.to( '.lay06', 1 , { scale: 1.01, autoAlpha: 0 }, 'startFade' );
+scroll.to( '.lay01', { scale: 1.2, y: 175, autoAlpha: 1, duration: 3 } )
+.to( '.lay02', { scale: 1.05, y: 150, autoAlpha: 1, duration: 3 }, 0 )
+.to( '.lay03', { y: -80, autoAlpha: 1, duration: 3 }, 3 )
+.to( '.lay03', { delay: 2, y: 0, autoAlpha: 0, duration: 6 }, 4 )
+.to( '.lay03-2', { delay: 6, y: -80, autoAlpha: 1, duration: 3 }, 6 )
+.to( '.lay03-2', { delay: 1, y: 0, autoAlpha: 0, duration: 6 }, 7 )
+.to( '.lay04', { scale: 1.025, y: 5, autoAlpha: 1, duration: 1 }, 0 )
+.to( '.moon', { delay: 10, scale: 0.8, x: 50, y: 100, duration: 3 }, 9 )
+.to( '.lay05', { scale: 1.02, autoAlpha: 1, duration: 1 }, 0 )
+.to( '.lay06', { scale: 1.02, autoAlpha: 1, duration: 1 }, 0 )
+.to( '.lay01', { delay: 4, scale: 1.2, y: 375, autoAlpha: 1, duration: 3 }, 5 )
+.to( '.lay02', { delay: 4, scale: 1.05, y: 350, autoAlpha: 1, duration: 3 }, 5 )
+.to( '.lay04', { delay: 4, scale: 1.2, x: -150, autoAlpha: 1, duration: 3 }, 5 );
+// .add('startFade')
+// .to( '#pinSection', 0 , { autoAlpha: 0 }, 'startFade' )
+// .to( '.lay01', 0 , { autoAlpha: 0 }, 'startFade' )
+// .to( '.lay02', 0 , { autoAlpha: 0 }, 'startFade' )
+// .to( '.lay03', 0 , { autoAlpha: 0 },  'startFade' )
+// .to( '.lay04', 0 , { autoAlpha: 0 }, 'startFade' )
+// .to( '.lay05', 0 , { autoAlpha: 0 }, 'startFade' )
+// .to( '.moon', 0, {  autoAlpha: 0 })
+// .to( '.lay06', 0 , { autoAlpha: 0 }, 'startFade' );
 
 new ScrollMagic.Scene({
     triggerElement: "#triggerPoint",
     triggerHook: 0,  // 改藍指標 0~1  // 0:最上方 | 1:最下方，一到頁面就觸發
-    duration: "600%"
+    duration: "500%"
 })
 .setPin("#pinSection")
 .setTween(scroll)
 .addIndicators()
 .addTo(controller);
+
+// parallax.js
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+// parallaxInstance = new Parallax( document.getElementById( "【物件】" ) , { 
+//     // 參數設定
+// });
--- js/teacher.js
+++ js/teacher.js
@@ -1,9 +1,23 @@
-$(function(){
+$(document).ready(function(){
     $( "#scrollPage" ).fullpage({
         // 參數設定
         navigation: true, // 顯示導行列
         navigationPosition: "right", // 導行列位置
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],  // 指定每個 section 的頁內位置  // → http://127.0.0.1:5502/teacher.html#firstPage
-        navigation: true,
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],  // 定義導航的錨文字 // → http://127.0.0.1:5502/teacher.html#firstPage
+        // navigationTooltips: ['A老師', 'B老師', 'C老師', 'D老師', 'E老師', 'F老師'],
+        // showActiveTooltip: true,
     });
 });
+
+// $(document).on('click', '.report', function(){  //使用$(document).on()的原因是如果id為submit的按鈕是一開始沒有載入、透過ajax互動後才產生的DOM，那用$().click會抓不到，需以$(document).on()才行
+//     var name = $('#reportReason').val();
+//     $.ajax({
+//         url :'../../php/report.php',
+//         method: 'POST',
+//         data: {
+//           name: name
+//         },
+//     //    success:function(res){
+//     //     }
+//     })
+// })
