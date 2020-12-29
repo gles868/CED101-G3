64e0d0e2b2afdcc27bff4ad0058884d2ce547463
Chusiuyi <siuyichu@gmail.com>
2020-12-29 12:08:38 +0800

    no message

--- js/course/singleCourse.js
+++ js/course/singleCourse.js
@@ -16,7 +16,7 @@ $(".heart").click(function () {
 
 //動畫效果
 gsap.to('.teacher_img', {y: 30, duration: 2,  ease: Power1.easeInOut,repeat: -1, yoyo: true});
-// gsap.to('.light', {rotation: 360, duration: 10,ease: Power0.easeInOut,repeat: -1, zIndex: -5});
+gsap.to('.light', {rotation: 360, duration: 10,ease: Power0.easeInOut,repeat: -1, zIndex: -5});
 gsap.to('.course_card', {y: 10, duration: 2, ease: Power1.easeInOut,repeat: -1, yoyo: true, zIndex: 10});
 
 
