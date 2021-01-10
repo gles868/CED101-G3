1aed7854656ab7e259aabd2c2638ac8e6a745083
Chusiuyi <siuyichu@gmail.com>
2021-01-10 19:12:19 +0800

    從資料庫撈資料，產生課程卡片

---
+++ connectced101g3_test.php
@@ -0,0 +1,7 @@
+<?php 
+	$dsn = "mysql:host=localhost;port=8888;dbname=ced101g3_test;charset=utf8";
+	$user = "root";
+	$password = "root";
+	$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
+	$pdo = new PDO($dsn, $user, $password, $options);
+?>
