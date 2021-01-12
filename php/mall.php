730ff461164c7891e5127769ff00afdb5b8429e7
m3m6owo <m3m6owo@gmail.com>
2021-01-12 12:55:14 +0800

    圖片全上

---
+++ php/mall.php
@@ -0,0 +1,28 @@
+<?php
+try {
+    require_once "./connect_ced101g3_test.php";
+
+    $sql = "select *
+            from product";
+    $memberdata = $pdo->query($sql);
+    // $managerdata = $pdo->prepare($sql);
+    // $managerdata->execute();
+
+    if ($memberdata->rowCount() == 0) { //找不到
+        //傳回空的JSON字串
+        echo "{}";
+
+    } else { //找得到
+        //取回一筆資料
+        $memberdatarow = $memberdata->fetchAll(PDO::FETCH_ASSOC);
+
+        //送出json字串
+        echo json_encode($memberdatarow);
+        // echo $managerdatarow;
+    }
+
+} catch (PDOException $e) {
+    //echo "系統錯誤, 請通知系維護人員~<br>";
+    echo "錯誤行號 : " . $e->getLine() . "<br>";
+    echo "錯誤原因 : " . $e->getMessage() . "<br>";
+}
