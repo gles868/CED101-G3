f6a5ac387064f78756b6d0b469dda90bd378edfe
yunyu_y <yuyang50513@gmail.com>
2021-01-17 19:52:33 +0800

    刪除舊的connected檔

--- connectced101g3_test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$dsn = "mysql:host=localhost;port=3306;dbname=ced101g3_test;charset=utf8";
-	$user = "root";
-	$password = "root";
-	$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
-	$pdo = new PDO($dsn, $user, $password, $options);
-?>
--- php/connect_ced101g3_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbname = "ced101g3_test";
-$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
-$user = "root";
-$password = "root";
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
-$pdo = new PDO($dsn, $user, $password, $options)
-?>
