0daffa9533046f760e1dad738f63b147de477afa
Chusiuyi <siuyichu@gmail.com>
2021-01-22 17:41:35 +0800

    no message

--- php/courseInsert.php
+++ php/courseInsert.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,9 +18,10 @@ try {
 			$products = $pdo->prepare( $sql );
 			$products -> bindValue(":memberNo", $_POST["memberNo"]);
 			$products -> bindValue(":classNo", $_POST["classNo"]);
-			$products -> execute();			
-			echo "新增成功~";
-	
+			$products -> execute();	
+			echo "報名成功";
+			// sleep(1);
+			// header('Location: http://localhost:8888/main.html');		
 
 
 } catch (PDOException $e) {
@@ -35,33 +35,3 @@ try {
 ?>    
 </body>
 </html>
-=======
-
-<?php
-$errMsg = "";
-try {
-	require_once("./connectced101g3_test.php");
-
-	//.......確定是否上傳成功
-
-	
-			$sql = "INSERT INTO `registration` (`memberNo`, `classNo`) 
-                                    values(:memberNo, :classNo)";
-			$products = $pdo->prepare( $sql );
-			$products -> bindValue(":memberNo", $_POST["memberNo"]);
-			$products -> bindValue(":classNo", $_POST["classNo"]);
-			$products -> execute();			
-			echo "新增成功~";
-	
-
-
-} catch (PDOException $e) {
-	// $pdo->rollBack();
-	$errMsg .= "錯誤原因 : ".$e -> getMessage(). "<br>";
-	$errMsg .= "錯誤行號 : ".$e -> getLine(). "<br>";	
-	echo $errMsg;
-}
-
-
-?>    
->>>>>>> chu
--- php/getLovedCourse.php
+++ php/getLovedCourse.php
@@ -1,11 +1,7 @@
 <?php
 require_once("connect_ced101g3.php");
-<<<<<<< HEAD
-$sql = "select * from course_list where memberNo = 3"; 
-=======
 session_start();
 $sql = "select * from course_list where memberNo = ?"; 
->>>>>>> chu
 $getlovedCourse = $pdo->prepare($sql);
 $getlovedCourse->bindValue(1, $_SESSION["memberNo"]);
 $getlovedCourse->execute();
