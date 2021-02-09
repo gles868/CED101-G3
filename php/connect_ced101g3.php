6cf85e5b9e573a13f2a9be4f6dbcd69025288902
yunyu-y <69347539+yunyu-y@users.noreply.github.com>
2021-02-09 15:07:25 +0800

    Delete connect_ced101g3.php

--- php/connect_ced101g3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbname = "tibamefe_ced101g3";
-$dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
-$user = "tibamefe_since2021";
-$password = "vwRBSb.j&K#E";
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => PDO::CASE_NATURAL);
-$pdo = new PDO($dsn, $user, $password, $options)
-?>
